2aecdda339159b86cf396a9bac82b8d6cb929341
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-02-09 17:46:37 +0200

    populate homepage

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,20 +1,43 @@
 import './App.css';
+import {useEffect} from 'react'
 import { Routes, Route} from "react-router-dom";
 import About from "./routes/AboutPage";
 import Home from "./routes/HomePage";
 import NavbarPage from './components/NavbarPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Intro from './components/Intro';
+import Skills from './components/Skills';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'Brye Grahn';
+    AOS.init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <NavbarPage />
-       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      </header>
+    <div className="px-6 lg:px-20 xl:px-36 bg-dark-500">
+      <Navbar />
+      <Intro />
+      <Skills />
+      {/* <Honors /> */}
+      {/* <Certs /> */}
+      <Footer />
     </div>
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //    <NavbarPage />
+    //    <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //   </Routes>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ my-portfolio/src/components/HR.js
@@ -0,0 +1,7 @@
+
+import hr from '../assets/hr.png'
+export default function HR(){
+    return (
+        <img src={hr} className="w-full md:h-3 mt-8" alt="hr" />
+    )
+}
