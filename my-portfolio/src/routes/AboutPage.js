c008ccdfe0b6abbf31c47ec3077f3b7bf6500d5c
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-02-09 12:53:52 +0200

    populate about page

---
+++ my-portfolio/src/routes/AboutPage.js
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                About
+            </h1>
+        </div>
+    )
+}
+export default AboutPage;
