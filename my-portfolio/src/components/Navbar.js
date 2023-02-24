10ecbe546cd759da0a8d400aa5cc9271918ca0b8
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-02-24 10:41:16 +0200

    remove bottom padding from navbar

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -5,9 +5,9 @@ import { Dropdown } from 'flowbite-react';
 export default function Navbar() {
     return (
         <div className ="bg-dark-1 ">
-        <div className="p-5 border-gray-200 rounded">
+        <div className="px-5 pt-5 border-gray-200 rounded">
             <nav class="">
-                <div className="p-3 shadow-lg px-6 lg:px-20  bg-dark-10 rounded-2xl">
+                <div className="p-1 shadow-lg px-6 lg:px-20  bg-dark-10 rounded-2xl">
                     <div className="flex justify-between items-center text-white">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-columns-gap" viewBox="0 0 16 16">
                             <path d="M6 1v3H1V1h5zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1H1zm14 12v3h-5v-3h5zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5zM6 8v7H1V8h5zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1H1zm14-6v7h-5V1h5zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1h-5z" />
