920f8efb78b9a18b8fa55c65caa800b59e14eba5
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-02-25 15:46:01 +0200

    style pages to match glass theme

--- my-portfolio/src/components/Intro.js
+++ my-portfolio/src/components/Intro.js
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import profile from '../assets/profile.svg';
+import spring from '../assets/spring.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCircleArrowRight, } from "@fortawesome/free-solid-svg-icons";
 import {  faGithub, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
@@ -19,14 +20,14 @@ export default function Intro () {
         }
         <div id="home" className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative">
             <div className='md:w-3/6 md:p-4'>
-                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)} />
+                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={spring} alt="profile" onLoad={() => setLoaded(false)} />
             </div>
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
                 <div className="flex flex-col w-full mt-8">
-                    <h1 className="text-xl text-gray-400">Hi, I'm</h1>
+                    <h1 className="text-xl text-gray-200">Hi, I'm</h1>
                     <h1 className="text-2xl font-bold">Bryce Grahn</h1>
-                    <p class="text-xl font-bold text-gray-300">Information Engineer</p>
-                    <p className="text-md font-light text-gray-400 ">Enthusiastic and motivated self-learning in Information Technology. Interested in exploring about Computer Network, Cloud Computing, DevSecops, and Web Development. Now I am looking for more experience in my field.</p>
+                    <p class="py-5 text-xl font-bold text-gray-100">Information Engineer</p>
+                    <p className="text-md font-light text-gray-200 ">Enthusiastic and motivated self-learning in Information Technology. Interested in exploring about Computer Network, Cloud Computing, DevSecops, and Web Development. Now I am looking for more experience in my field.</p>
                 </div>
                 <a href='http://blog.dama.ink' className='mt-2 block'>Go to My Blog <FontAwesomeIcon className='ml-2' icon={faCircleArrowRight}/> </a>     
                 <ul className='flex mt-2 gap-4 items-center'>
--- my-portfolio/src/components/Skills.js
+++ my-portfolio/src/components/Skills.js
@@ -35,7 +35,7 @@ export default function Skills() {
     return (
         <div id="skills" className="mt-4 text-white">
             <h1 className="text-2xl font-bold">Skills</h1>
-            <p className="font-light text-gray-400">Here are some of my skills</p>
+            <p className="font-light text-gray-200">Here are some of my skills</p>
 
             <div className="mt-4">
                 <Slider {...settings}>
