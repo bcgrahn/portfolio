a61bf972ff1286a9115fd101ca5672cb57956c1e
Bryce <99645139+bcgrahn@users.noreply.github.com>
2023-03-08 21:11:43 +0200

    close ham-menue after selection

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -65,14 +65,14 @@ export default function Navbar() {
                     <div className="">
                         <ul class="primary-navigation hamGlassCard ">
                             <li className="p-1 pt-4">
-                                <a href="#home" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
+                                <a onClick={toggleDiv} href="#home" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="inline-block bi bi-person-vcard-fill" viewBox="0 0 16 16">
                                         <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm9 1.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4a.5.5 0 0 0-.5.5ZM9 8a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4A.5.5 0 0 0 9 8Zm1 2.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5Zm-1 2C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 0 2 13h6.96c.026-.163.04-.33.04-.5ZM7 6a2 2 0 1 0-4 0 2 2 0 0 0 4 0Z" />
                                     </svg> &nbsp; About
                                 </a>
                             </li>
                             <li className="p-1">
-                                <a href="#skills" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
+                                <a onClick={toggleDiv} href="#skills" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="inline-block bi bi-lightbulb" viewBox="0 0 16 16">
                                         <path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13a.5.5 0 0 1 0 1 .5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1 0-1 .5.5 0 0 1 0-1 .5.5 0 0 1-.46-.302l-.761-1.77a1.964 1.964 0 0 0-.453-.618A5.984 5.984 0 0 1 2 6zm6-5a5 5 0 0 0-3.479 8.592c.263.254.514.564.676.941L5.83 12h4.342l.632-1.467c.162-.377.413-.687.676-.941A5 5 0 0 0 8 1z" />
                                     </svg>
@@ -80,7 +80,7 @@ export default function Navbar() {
                                 </a>
                             </li>
                             <li className="p-1">
-                                <a href="#honors" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
+                                <a onClick={toggleDiv} href="#honors" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="inline-block bi bi-award" viewBox="0 0 16 16">
                                         <path d="M9.669.864 8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193.684 1.365 1.086 1.072L12.387 6l.248 1.506-1.086 1.072-.684 1.365-1.51.229L8 10.874l-1.355-.702-1.51-.229-.684-1.365-1.086-1.072L3.614 6l-.25-1.506 1.087-1.072.684-1.365 1.51-.229L8 1.126l1.356.702 1.509.229z" />
                                         <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z" />
@@ -89,7 +89,7 @@ export default function Navbar() {
                                 </a>
                             </li>
                             <li className="p-1">
-                                <a href="#certs" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
+                                <a onClick={toggleDiv} href="#certs" className="text-sm block py-1 pl-3 pr-4 text-gray-200 rounded hover:bg-gray-100">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="inline-block bi bi-mortarboard" viewBox="0 0 16 16">
                                         <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5ZM8 8.46 1.758 5.965 8 3.052l6.242 2.913L8 8.46Z" />
                                         <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Zm-.068 1.873.22-.748 3.496 1.311a.5.5 0 0 0 .352 0l3.496-1.311.22.748L8 12.46l-3.892-1.556Z" />
@@ -97,7 +97,7 @@ export default function Navbar() {
                                     &nbsp; Certfications
                                 </a>
                             </li>
-                            <li className="pt-10 pb-2">
+                            <li onClick={toggleDiv} className="pt-10 pb-2">
                                 <a href={resume} rel="noreferrer" target="_blank" className="">
                                     <button class="glass-button">Resume</button></a>
                             </li>
